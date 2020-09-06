e12e3590b4ac08e2c7a4034aed57caed18095889
Sang Dang <sang.a5vy2@gmail.com>
2020-09-06 11:29:29 +0700

    Switch mongodb form mLab to mongo Atlas

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.providers';
 import { MongooseModule } from '@nestjs/mongoose';
-const mongoConfig = 'mongodb://heroku_nz29xcb8:agud4d14kekc2955rgqroci69h@ds213896.mlab.com:13896/heroku_nz29xcb8';
+const mongoConfig =
+  'mongodb+srv://sangdangq:nashtechfe@cluster0.dasdv.mongodb.net/nest?retryWrites=true&w=majority';
 @Module({
   imports: [MongooseModule.forRoot(mongoConfig)],
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
-
 export class DatabaseModule {}
